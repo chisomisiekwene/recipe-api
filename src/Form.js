dbecc07486b0ad73c3f042070184208c68428110
chisomisiekwene <chisomisiekwene57@gmail.com>
2023-01-05 12:22:47 +0100

    setup the codebase

---
+++ src/Form.js
@@ -0,0 +1,10 @@
+function Form(){
+    return(
+        <form id="search-form" class="search-form">
+            <input type="text" id="search-input" class="search-input" placeholder="Search for a recipe"/>
+            <button type="submit" class="search-button" id="search-btn">Search</button>
+        </form>
+    )
+    
+}
+export default Form;
